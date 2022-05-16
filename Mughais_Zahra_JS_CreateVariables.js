5e8cb6f1bf7ec2ded6cc4cfac64a6ea7a74cb81c
Zahra Mughais <zahramughais@gmail.com>
2022-05-16 15:59:07 +0300

    Creating variables

---
+++ Mughais_Zahra_JS_CreateVariables.js
@@ -0,0 +1,11 @@
+//variables 
+
+//to save the rider height
+var height = 0 ;
+//to save the rider age
+var age = 0 ;
+// at least 52 inches tall 
+var minHeight = 42;
+// 10 years and older
+var minAge = 10;
+
